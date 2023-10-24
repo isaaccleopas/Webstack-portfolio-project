fdc5380e081b0075b17414aa9b0d5643a213c530
isaaccleopas <isaaccleopas@gmail.com>
2023-10-24 08:06:46 +0000

    Removed frontend directory

--- frontend/script/app.js
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-// declarations
-const signUpForm = document.querySelector('.signUp-form');
-const signInForm = document.querySelector('.signIn-form');
-const listsForm = document.querySelector('.lists-form');
-
